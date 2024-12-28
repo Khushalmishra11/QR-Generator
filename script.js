a62076c02184f643ae0d4aa599f83f52f2c28076
Khushal Mishra <khushalmishra261203@gmail.com>
2024-12-28 23:49:43 +0530

    updated

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 
+let qr = document.getElementById("qr");
+let qr-img = document.getElementById("qr-img");
+let input = document.getElementById("input");
+
 
 function generate(){
     
-    const url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=hello"
+    qr-Image.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + input.value;
 }
+
+const btn = document.getElementById("btn").onclick = generate();
